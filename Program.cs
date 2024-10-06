38053e909367efa154d1c65c35a797097d1ff9cb
SebitasDev <sramireg13@gmail.com>
2024-10-05 21:02:56 -0500

    s

--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("NewPolicy", policy =>
@@ -23,7 +21,7 @@ builder.Services.AddCors(options =>
     });
 });
 
-
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
