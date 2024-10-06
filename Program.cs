f7bb9c745e4b0cc77c980abb68cbfe2bff18aa50
SebitasDev <sramireg13@gmail.com>
2024-10-05 20:56:13 -0500

    f

--- Program.cs
+++ Program.cs
@@ -26,11 +26,8 @@ builder.Services.AddCors(options =>
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
